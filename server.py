d1422fdd2c2dfaf895f6575b9d38902b6d325046
zehranurugurr <221118064@samsun.edu.tr>
2025-06-04 14:46:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 from mcp.server.fastmcp import FastMCP
-from app import get_ghibli_films  # film listesini döndüren fonksiyon
+from app import get_ghibli_films  # Yukarıdaki fonksiyon burada çağrılıyor
 
-# MCP server başlatılıyor
 mcp = FastMCP("films-mcp")
 
 @mcp.tool()
-async def get_films() -> dict:
-    """
-    Get a list of Ghibli films.
-    """
+async def get_films(name: str) -> dict:
     films = get_ghibli_films()
+
+    if isinstance(films, dict) and "error" in films:
+        return films
+
+    if name:
+        result = [f for f in films if name.lower() in f["title"].lower()]
+        return {"films": result or f"'{name}' adlı film bulunamadı."}
+
     return {"films": films}
 
 if __name__ == "__main__":
